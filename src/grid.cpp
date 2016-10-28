31db01200d9c4917039fb568c6987681afe94f04
schroeji <schroeji@studi.informatik.uni-stuttgart.de>
2016-10-28 10:33:43 +0200

    Implementierung von Iterator und Teile von Grid

--- src/grid.cpp
+++ src/grid.cpp
@@ -1,1 +1,70 @@
+#include <algorithm>
+#include "stdlib.h"
+#include "math.h"
+
 #include "grid.hpp"
+#include "geometry.hpp"
+#include "iterator.hpp"
+
+Grid::Grid(const Geometry *geom){
+  multi_index_t geom_size = geom->Size();
+  _geom = geom;
+  _data = (real_t*) malloc(geom_size[0] * geom_size[1] * sizeof(real_t));
+  _offset = {0.0, 0.0};
+}
+
+Grid::Grid(const Geometry *geom, const multi_real_t &offset) {
+  multi_index_t geom_size = geom->Size();
+  _geom = geom;
+  _data = (real_t*) malloc(geom_size[0] * geom_size[1] * sizeof(real_t));
+  _offset = offset;
+}
+
+void Grid::Initialize(const real_t &value) {
+  Iterator it(_geom);
+  for (it.First(); it.Valid(); it.Next()) {
+    _data[it] = value;
+  }
+}
+
+real_t& Grid::Cell(const Iterator &it) {
+  return _data[it];
+}
+
+const real_t& Grid::Cell(const Iterator &it) const {
+  return _data[it];
+}
+
+real_t Grid::Max() const {
+  Iterator it(_geom);
+  real_t max_val = _data[0];
+  for (it.First(); it.Valid(); it.Next()) {
+    max_val = std::max(max_val, _data[it]);
+  }
+  return max_val;
+}
+
+real_t Grid::Min() const {
+  Iterator it(_geom);
+  real_t min_val = _data[0];
+  for (it.First(); it.Valid(); it.Next()) {
+    min_val = std::min(min_val, _data[it]);
+  }
+  return min_val;
+}
+
+real_t Grid::AbsMax() const {
+  Iterator it(_geom);
+  real_t max_val = fabs(_data[0]);
+  for (it.First(); it.Valid(); it.Next()) {
+    max_val = std::max(max_val, fabs(_data[it]));
+  }
+  return max_val;
+}
+
+real_t* Grid::Data(){
+  return _data;
+}
+Grid::~Grid(){
+  free (_data);
+}
--- src/iterator.cpp
+++ src/iterator.cpp
@@ -1,5 +1,87 @@
 #include "iterator.hpp"
+#include "geometry.hpp"
 
 Iterator::Iterator(const Geometry *geom){
+  _geom = geom;
+  _value = 0;
+  const multi_index_t& geom_size = geom->Size();
+  _valid = (geom_size[0] > 0) && (geom_size[1] > 0);
+}
 
+Iterator::Iterator(const Geometry *geom, const index_t &value){
+  _geom = geom;
+  _value = value;
+  const multi_index_t& geom_size = geom->Size();
+  _valid = (geom_size[0] > value) && (geom_size[1] > value);
+}
+
+const index_t& Iterator::Value() const{
+  return _value;
+}
+
+Iterator::operator const index_t& () const {
+  return _value;
+}
+
+multi_index_t Iterator::Pos() const{
+  index_t row_len = _geom->Size()[0];
+  index_t ind1 = _value % row_len;
+  index_t ind2 = _value / row_len;
+  multi_index_t ret = {ind1, ind2};
+  return ret;
+}
+
+void Iterator::First() {
+  _value = 0;
+}
+
+void Iterator::Next() {
+  ++_value;
+}
+
+bool Iterator::Valid() const {
+  const multi_index_t& geom_size = _geom->Size();
+  return _value < geom_size[0]*geom_size[1];
+}
+
+Iterator Iterator::Left() const {
+  multi_index_t pos = Pos();
+  if(pos[0] == 0){
+    return Iterator(_geom, _value);
+  }
+  else{
+    return Iterator(_geom, _value - 1);
+  }
+}
+
+Iterator Iterator::Right() const {
+  const multi_index_t& geom_size = _geom->Size();
+  multi_index_t pos = Pos();
+  if(pos[0] == geom_size[1] - 1){
+    return Iterator(_geom, _value);
+  }
+  else{
+    return Iterator(_geom, _value + 1);
+  }
+}
+
+Iterator Iterator::Top() const {
+  multi_index_t pos = Pos();
+  if(pos[1] == 0){
+    return Iterator(_geom, _value);
+  }
+  else{
+    return Iterator(_geom, _value - _geom->Size()[0]);
+  }
+}
+
+Iterator Iterator::Down() const {
+  const multi_index_t& geom_size = _geom->Size();
+  multi_index_t pos = Pos();
+  if(pos[1] == geom_size[1]){
+    return Iterator(_geom, _value);
+  }
+  else{
+    return Iterator(_geom, _value - geom_size[0]);
+  }
 }
