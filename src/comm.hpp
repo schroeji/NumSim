1663325ec24788d77d057473e8aeb3811122bbbd
schroeji <schroeji@studi.informatik.uni-stuttgart.de>
2016-11-24 12:23:39 +0100

    parallelisierungs header

---
+++ src/comm.hpp
@@ -0,0 +1,44 @@
+#include "grid.hpp"
+#include "typedef.hpp"
+//------------------------------------------------------------------------------
+#ifndef __COMM_HPP
+#define __COMM_HPP
+//------------------------------------------------------------------------------
+class Communicator {
+public:
+	Communicator (int* argc, char*** argv);
+	~Communicator ();
+
+    const multi_index_t& ThreadIdx () const;
+    const multi_index_t& ThreadDim () const;
+
+    const int&	ThreadNum	() const;
+    const int&	ThreadCnt	() const;
+
+    const bool&	EvenOdd		() const;
+
+    real_t	geatherSum		(const real_t& val) const;
+    real_t	geatherMin		(const real_t& val) const;
+    real_t	geatherMax		(const real_t& val) const;
+
+	void	copyBoundary	(Grid* grid) const;
+
+	const bool isLeft () const;
+	const bool isRight () const;
+	const bool isTop () const;
+	const bool isBottom () const;
+private:
+	multi_index_t _tidx;
+	multi_index_t _tdim;
+	int _rank;
+	int _size;
+	bool _evenodd;
+
+	bool copyLeftBoundary	(Grid* grid) const;
+    bool copyRightBoundary	(Grid* grid) const;
+    bool copyTopBoundary	(Grid* grid) const;
+    bool copyBottomBoundary	(Grid* grid) const;
+};
+//------------------------------------------------------------------------------
+#endif // __COMM_HPP
+//------------------------------------------------------------------------------
