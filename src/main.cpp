2f807b67cdf1e1d9bef94c47b1b569e19a779b97
schmidtal <alexander.schmidt@eclipso.de>
2016-12-04 10:35:02 +0100

    adapt visu range

--- src/main.cpp
+++ src/main.cpp
@@ -65,16 +65,18 @@ int main(int argc, char **argv) {
 
   // Create parameter and geometry instances with default values
   Parameter param;
-  Geometry geom(&communicator);
+  Geometry geom( &communicator );
   // Create the fluid solver
 
-  Compute comp( &geom, &param, &communicator);
-  // Compute comp( &geom, &param, nullptr);
+  // Compute comp( &geom, &param, &communicator);
+  Compute comp( &geom, &param, &communicator );
   // testIterator(&geom);
 #ifdef USE_DEBUG_VISU
  // Create and initialize the visualization
  Renderer visu(geom.Length(), geom.Mesh());
- visu.Init(800, 800);
+ visu.Init( 600 / communicator.ThreadDim()[0],
+           (int)(600 * geom.TotalLength()[1] / geom.TotalLength()[0]) / communicator.ThreadDim()[1],
+           communicator.ThreadNum());
 #endif // USE_DEBUG_VISU
 
   // Create a VTK generator
@@ -109,7 +111,7 @@ int main(int argc, char **argv) {
 #endif // DEBUG_VISU
 
     // Create a VTK File in the folder VTK (must exist)
-    vtk.Init("VTK/");
+    vtk.Init("VTK/field");
     vtk.AddField("Velocity", comp.GetU(), comp.GetV());
     vtk.AddScalar("Pressure", comp.GetP());
 
@@ -118,7 +120,7 @@ int main(int argc, char **argv) {
 
     // std::cin.get();
     // Run a few steps
-    for (uint32_t i = 0; i < 9; ++i)
+    for (uint32_t i = 0; i < 20; ++i)
       comp.TimeStep(false);
     comp.TimeStep(true);
     // comp.TimeStep(true);
